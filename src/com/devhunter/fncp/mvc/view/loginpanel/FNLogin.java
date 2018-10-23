e54082e84410bfdd44ba192cd9998176b2bdd327
inertBear <31257145+inertBear@users.noreply.github.com>
2018-10-23 12:45:17 -0500

    [FNCP-031] import libs / login logic

--- src/com/devhunter/fncp/mvc/view/loginpanel/FNLogin.java
+++ src/com/devhunter/fncp/mvc/view/loginpanel/FNLogin.java
@@ -1,5 +1,5 @@
 /**
- * � 2017-2018 FieldNotes
+ * ? 2017-2018 FieldNotes
  * All Rights Reserved
  * <p>
  * Created by DevHunter exclusively for FieldNotes
@@ -9,18 +9,21 @@ package com.devhunter.fncp.mvc.view.loginpanel;
 
 import com.devhunter.fncp.FNInit;
 import com.devhunter.fncp.constants.FNConstants;
+import com.devhunter.fncp.mvc.controller.JsonParser;
 import com.devhunter.fncp.mvc.controller.sql.FNLoginController;
 import com.devhunter.fncp.mvc.model.fnview.*;
 import com.devhunter.fncp.mvc.view.FNControlPanel;
 import com.devhunter.fncp.utilities.FNUtil;
-import com.mysql.jdbc.exceptions.jdbc4.CommunicationsException;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This class draws the login panel as a singleton. Successful login will create an instance of
@@ -46,6 +49,8 @@ public class FNLogin extends FNPanel {
     // ImageButtons
     private FNImageButton mButtonLogin;
 
+    private JsonParser mJsonParser = new JsonParser();
+
     private FNLogin() {
         // Create Frames
         mLoginFrame = FNInit.getFieldNotesJFrame();
@@ -67,85 +72,122 @@ public class FNLogin extends FNPanel {
     }
 
     public void init() {
-        // Frame
-        mLoginFrame.setSize(FNConstants.lOGIN_PANEL_X_AXIS, FNConstants.lOGIN_PANEL_Y_AXIS);
-        mLoginFrame.setLocationRelativeTo(null);
-        mLoginFrame.setVisible(true);
-        mLoginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // Panels/Layouts
-        BorderLayout loginPanelMainLayout = new BorderLayout();
-        mLoginPanel.setLayout(loginPanelMainLayout);
-        GridLayout loginCredentialPanelLayout = new GridLayout(0, 2);
-        mLoginCredentialPanel.setLayout(loginCredentialPanelLayout);
-        // ImageLabels
+        //get product key from saved file
+        String path = System.getProperty("user.home") + File.separator + "Documents";
+        File textFile = new File(path, "FNCP_product_key.txt");
+        String productKey = "";
+
+        FileReader fileReader;
         try {
-            Image img = ImageIO.read(getClass().getResource(FNConstants.APPLICATION_LOGO_FOLDER));
-            mLoginTitleLabel = new FNImageLabel(new ImageIcon(img));
-        } catch (Exception ex) {
-            JOptionPane.showMessageDialog(mLoginFrame, "title image failed to load");
+            fileReader = new FileReader(textFile);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            productKey = bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        // ImageButtons
-        try {
-            Image img = ImageIO.read(getClass().getResource(FNConstants.BUTTON_SUBMIT_LOCATION));
-            mButtonLogin = new FNImageButton(new ImageIcon(img));
-        } catch (IOException e1) {
-            JOptionPane.showMessageDialog(mLoginFrame, "Image button failed to load");
-        }
-        // Labels
-        FNLabel lblUsername = new FNLabel(FNConstants.CRED_USERNAME_LABEL);
-        FNLabel lblPassword = new FNLabel(FNConstants.CRED_PASSWORD_LABEL);
-        // Override Label properties
-        mLoginUsername.setPreferredSize(FNUtil.getInstance().getStandardTextFieldDimension());
-        mLoginPassword.setPreferredSize(FNUtil.getInstance().getStandardTextFieldDimension());
-        // Override PasswordField properties
-        mLoginPassword.setBorder(BorderFactory.createLineBorder(FNUtil.getInstance().getPrimaryColor()));
 
-        // Add Views to UserName/Password Panels
-        mLoginCredentialPanel.add(lblUsername);
-        mLoginCredentialPanel.add(mLoginUsername);
-        mLoginCredentialPanel.add(lblPassword);
-        mLoginCredentialPanel.add(mLoginPassword);
-        // Add Views to Main Panel
-        mLoginPanel.add(mLoginTitleLabel, BorderLayout.NORTH);
-        mLoginPanel.add(mLoginCredentialPanel, BorderLayout.CENTER);
-        mLoginPanel.add(mButtonLogin, BorderLayout.SOUTH);
-        mLoginPanel.setVisible(true);
-        // Add Main Panel to Frame
-        mLoginFrame.add(mLoginPanel, BorderLayout.CENTER);
-        mLoginFrame.repaint();
-        mLoginFrame.revalidate();
-        //pressing "Enter" triggers the Login Button
-        JRootPane rootPane = SwingUtilities.getRootPane(mButtonLogin);
-        rootPane.setDefaultButton(mButtonLogin);
+        //validate product key
+        boolean result;
+        FNLoginController action = new FNLoginController();
+        result = action.SQLRegister(productKey);
 
-        mButtonLogin.addActionListener(e -> {
-            mLoginPanel.setVisible(false);
-            String username = mLoginUsername.getText();
+        if (!result) {
+            FNRegister.getInstance();
+        } else {
+            // validation success, prompt login
 
-            if (username != null) {
+            // Frame
+            mLoginFrame.setSize(FNConstants.lOGIN_PANEL_X_AXIS, FNConstants.lOGIN_PANEL_Y_AXIS);
+            mLoginFrame.setLocationRelativeTo(null);
+            mLoginFrame.setVisible(true);
+            mLoginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            // Panels/Layouts
+            BorderLayout loginPanelMainLayout = new BorderLayout();
+            mLoginPanel.setLayout(loginPanelMainLayout);
+            GridLayout loginCredentialPanelLayout = new GridLayout(0, 2);
+            mLoginCredentialPanel.setLayout(loginCredentialPanelLayout);
+            // ImageLabels
+            try {
+                Image img = ImageIO.read(getClass().getResource(FNConstants.APPLICATION_LOGO_FOLDER));
+                mLoginTitleLabel = new FNImageLabel(new ImageIcon(img));
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(mLoginFrame, "title image failed to load");
+            }
+            // ImageButtons
+            try {
+                Image img = ImageIO.read(getClass().getResource(FNConstants.BUTTON_SUBMIT_LOCATION));
+                mButtonLogin = new FNImageButton(new ImageIcon(img));
+            } catch (IOException e1) {
+                JOptionPane.showMessageDialog(mLoginFrame, "Image button failed to load");
+            }
+            // Labels
+            FNLabel lblUsername = new FNLabel(FNConstants.CRED_USERNAME_LABEL);
+            FNLabel lblPassword = new FNLabel(FNConstants.CRED_PASSWORD_LABEL);
+            // Override Label properties
+            mLoginUsername.setPreferredSize(FNUtil.getInstance().getStandardTextFieldDimension());
+            mLoginPassword.setPreferredSize(FNUtil.getInstance().getStandardTextFieldDimension());
+            // Override PasswordField properties
+            mLoginPassword.setBorder(BorderFactory.createLineBorder(FNUtil.getInstance().getPrimaryColor()));
+
+            // Add Views to UserName/Password Panels
+            mLoginCredentialPanel.add(lblUsername);
+            mLoginCredentialPanel.add(mLoginUsername);
+            mLoginCredentialPanel.add(lblPassword);
+            mLoginCredentialPanel.add(mLoginPassword);
+            // Add Views to Main Panel
+            mLoginPanel.add(mLoginTitleLabel, BorderLayout.NORTH);
+            mLoginPanel.add(mLoginCredentialPanel, BorderLayout.CENTER);
+            mLoginPanel.add(mButtonLogin, BorderLayout.SOUTH);
+            mLoginPanel.setVisible(true);
+            // Add Main Panel to Frame
+            mLoginFrame.add(mLoginPanel, BorderLayout.CENTER);
+            mLoginFrame.repaint();
+            mLoginFrame.revalidate();
+            //pressing "Enter" triggers the Login Button
+            JRootPane rootPane = SwingUtilities.getRootPane(mButtonLogin);
+            rootPane.setDefaultButton(mButtonLogin);
+
+            mButtonLogin.addActionListener(e -> {
+                mLoginPanel.setVisible(false);
+
+                // set URL for web service
+                final String postUrl = "http://www.fieldnotesfn.com/FNA_test/FNA_login.php";
+
+                // get inputs from UI
+                String username = mLoginUsername.getText();
                 String password = mLoginPassword.getText();
-                // send FNUser to controller for login validation
-                boolean result;
-                FNLoginController action = new FNLoginController();
-                result = action.SQLLogin(username, password);
-                if (result) {
+
+                // convert to List of params
+                List<NameValuePair> params = new ArrayList<>();
+                params.add(new BasicNameValuePair("username", username));
+                params.add(new BasicNameValuePair("password", password));
+                params.add(new BasicNameValuePair("customerKey", "1159616266"));
+
+                // make HTTP connection
+                JSONObject json = mJsonParser.createHttpRequest(postUrl, "POST", params);
+                String status = json.getString("status");
+                String message = json.getString("message");
+
+                if (status.equals("success")) {
+                    //set username to session
+                    //find out if user has Admin Access
                     FNUtil.getInstance().setCurrentUser(username);
+                    //open Control Panel
                     FNControlPanel.getInstance();
-                    //mLoginFrame.dispatchEvent(new WindowEvent(mLoginFrame, WindowEvent.WINDOW_CLOSING));
                 } else {
-                    JOptionPane.showMessageDialog(mLoginFrame, "Invalid username or password");
+                    // display failure
+                    JOptionPane.showMessageDialog(mLoginFrame, message);
+
+                    //reset UI
                     mLoginPanel.setVisible(true);
                     mLoginPassword.setText("");
                 }
-            } else {
-                JOptionPane.showMessageDialog(mLoginFrame, "Please enter a username");
-                mLoginPanel.setVisible(true);
-                mLoginPassword.setText("");
-            }
-        });
+            });
+        }
     }
 
     public static JFrame getFieldNotesLoginFrame() {
         return mLoginFrame;
     }
 }
+
